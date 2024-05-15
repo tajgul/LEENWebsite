d082052fa8927003854adc08eb7f0d5627a1ba33
SartajGulati <Sartajsinghgulati@gmail.com>
2024-05-14 21:57:22 -0400

    Changed mobile

--- script.js
+++ script.js
@@ -32,3 +32,11 @@ window.addEventListener('scroll', function() {
         });
     });
 });
+
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
